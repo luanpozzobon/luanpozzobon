7eedd4cc2efc51bcbb535ac1739c81955506e93c
luanpozzobon <luanpozzobon@gmail.com>
2025-05-01 17:48:52 -0300

    feat(component): add 'hotbar' component
    This component will be visible in every screen, and will be used on the website navigation.

---
+++ web/components/hotbar/hotbar-component.js
@@ -0,0 +1,48 @@
+hotbar_slots = {
+    'home': 0,
+    'contact': 1
+}
+
+class Hotbar extends HTMLElement {
+    HTML_PATH = '/components/hotbar/hotbar-component.html';
+    CSS_PATH = '/components/hotbar/hotbar-component.css';
+
+    constructor() {
+        super();
+        this.attachShadow({
+            mode: 'open',
+        })
+    }
+
+    async connectedCallback() {
+        const html = await fetch(this.HTML_PATH)
+            .then(response => response.text());
+
+        const css = await fetch(this.CSS_PATH)
+            .then(response => response.text());
+
+        this.shadowRoot.innerHTML = `
+            <style>${css}</style>
+            ${html}
+        `;
+
+        window.addEventListener('hashchange', () => this.changeSlot(window.location.hash));
+        this.changeSlot(window.location.hash);
+    }
+
+    changeSlot(hash) {
+        const hotbar = this.shadowRoot.getElementById("hotbar");
+
+        const pathname = hash.replace('#', '');
+        const idx = hotbar_slots[pathname];
+
+        hotbar.querySelectorAll('slot-component').forEach(slot => {
+            slot.shadowRoot?.getElementById('slot')?.classList.remove('selected');
+        })
+        setTimeout(() => {
+            hotbar.querySelectorAll('slot-component')[idx]?.shadowRoot?.getElementById('slot')?.classList.add('selected');
+        }, 200)
+    }
+}
+
+customElements.define('hotbar-component', Hotbar);
