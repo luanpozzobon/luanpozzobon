8d9ac4fb6d800ee9301fae86f7f0368f91ed92b9
luanpozzobon <luanpozzobon@gmail.com>
2025-05-02 11:08:04 -0300

    feat(home): home page animation

--- web/app.js
+++ web/app.js
@@ -1,20 +1,44 @@
 const routes = {
     '#home': 'home',
     '#contact': 'contact',
-    '#about': 'about',
+    '#aboutme': 'aboutme',
 };
 
 async function loadPage(route) {
     const page = routes[route] || 'home';
 
     try {
-        const html = await fetch(`/pages/${page}/${page}.html`)
-            .then(response => response.text());
+        const response = await fetch(`/pages/${page}/${page}.html`);
+        if (!response.ok)
+            throw new Error('Página não encontrada');
 
+        const html = await response.text()
         document.getElementById('app').innerHTML = html
     } catch {
         document.getElementById('app').innerHTML = '<h2>Página não encontrada</h2>';
     }
+
+    if (page === 'home') animateWelcome()
+}
+
+function animateWelcome() {
+    const element = document.getElementById("welcome");
+    const welcomeText = element.textContent;
+    let i = 0;
+
+    element.textContent = "";
+
+    const MAX_TIMEOUT = 500;
+    function digitar() {
+        if (i < welcomeText.length) {
+            element.textContent += welcomeText.charAt(i);
+            i++;
+            let timeout = Math.floor(Math.random() * MAX_TIMEOUT)
+            setTimeout(() => digitar(), timeout)
+        }
+    }
+
+    digitar()
 }
 
 window.addEventListener('hashchange', () => loadPage(location.hash));
