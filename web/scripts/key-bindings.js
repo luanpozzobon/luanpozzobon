d522310d4aaeabca3c187197129f847bc36a9605
luanpozzobon <luanpozzobon@gmail.com>
2025-05-02 11:09:05 -0300

    feat(key-binding): better key-bindings

--- web/scripts/key-bindings.js
+++ web/scripts/key-bindings.js
@@ -1,8 +1,73 @@
 mapping = {
-    '1': () => window.location.hash = '#home',
-    '2': () => window.location.hash = '#contact',
+    '1': () => {
+        const link = getHotbarLink(1);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '2': () => {
+        const link = getHotbarLink(2);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '3': () => {
+        const link = getHotbarLink(3);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '4': () => {
+        const link = getHotbarLink(4);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '5': () => {
+        const link = getHotbarLink(5);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '6': () => {
+        const link = getHotbarLink(6);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+
+    '7': () => {
+        const link = getHotbarLink(7);
+        if (!link) return;
+
+        window.open(link.href, '_self');
+    },
+    '8': () => {
+        const link = getHotbarLink(8);
+        if (!link) return;
+
+        window.open(link.href, '_blank');
+    },
+    '9': () => {
+        const link = getHotbarLink(9);
+        if (!link) return
+
+        window.open(link.href, '_blank');
+    },
 }
 
 window.addEventListener('keypress', (event) => {
-    mapping[event.key]();
+    const keyMap = mapping[event.key.toUpperCase()];
+    if (!keyMap) return;
+
+    keyMap(keyMap.name);
 })
+
+function getHotbarLink(slotNumber) {
+    const LINK_PREFIX = 'hotbar-link';
+    const hotbar = document.querySelector('hotbar-component');
+    
+    if (!hotbar) return null;
+
+    return hotbar.shadowRoot.getElementById(`${LINK_PREFIX}-${slotNumber}`);
+}
